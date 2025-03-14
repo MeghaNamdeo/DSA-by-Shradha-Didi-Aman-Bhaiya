61a9eb129406e9acb3188b7e87f4e25c2d80d698
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2025-03-14 14:10:10 +0530

    Create 03_Maximum Subarray_or_Kadane's_algo.cpp

---
+++ 03_Maximum Subarray_or_Kadane's_algo.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int maxi = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+
+            for (int j = i; j < nums.size(); j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum = sum + nums[k];
+                    maxi = max(maxi, sum);
+                }
+            }
+        }
+        return maxi;
+    }
+};
+// tc: O(n^3)
+// sc: O(1)
+
+----------------------------------------------------------------------------
+  // better approach
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int maxi = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++) {
+
+                sum = sum + nums[j];
+                maxi = max(maxi, sum);
+            }
+        }
+        return maxi;
+    }
+};
+// tc: O(n^2)
+// sc: O(1)
+
+---------------------------------------------------------------------------------------
+  
